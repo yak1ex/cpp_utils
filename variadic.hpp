4dffdae96564d3d5544edbd2b04bf699b24dee8f
Yak! <yak_ex@mx.scn.tv>
2014-02-11 20:36:55 +0900

    Use forward.

--- variadic.hpp
+++ variadic.hpp
@@ -1,12 +1,14 @@
 #ifndef YAK_UTIL_VARIADIC_HPP
 #define YAK_UTIL_VARIADIC_HPP
 
+#include <utility>
+
 namespace yak {
 namespace util {
 
 bool any() { return true; }
 template<typename T0, typename ... T>
-bool any(T0 t0, T ... t) { return t0 | ::yak::util::any(t...); } 
+bool any(T0 && t0, T && ... t) { return t0 || ::yak::util::any(std::forward<T>(t)...); }
 
 } // namespace util
 } // namespace yak
